892d7347bbd638c29f5a1ca0c0595189bd9c09b4
InkAlan <cedrick151545@gmail.com>
2025-01-16 13:10:02 +0800

    header 100%

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const burgir = document.querySelector(".toggle__menu");
+const headerav = document.querySelector(".header .header__nav");
 burgir.addEventListener("click",() => {
 burgir.classList.toggle("open");
+headerav.classList.toggle("open");
  });
